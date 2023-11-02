134084ce22f6bf5bde0d909dce6342397e156f1f
htetnaing <htetnainglynnhnl@gmail.com>
2023-11-02 10:20:04 +0630

    create MainTestController and create MainTest table

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('main-test/create', function () {
     return view('mainTest.create');
 });
 
+Route::get('main-test/edit', function () {
+    return view('mainTest.edit');
+});
+
 Route::get('/', function () {
     return view('master.master');
 });
